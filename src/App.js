a5b9b649fe6859678208cabac319450245715f38
Wren <renprofessional97@gmail.com>
2022-06-09 18:00:08 +0100

    Finish for the day

--- src/App.js
+++ src/App.js
@@ -1,8 +1,64 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ToDo from "./components/TodoList";
 
-function App() {
-  const [toDos, setToDos] = useState
-}
+const App = () => {
+  const [task, setTask] = useState([]);
+  const [input, setInput] = useState("");
+  const submitHandler = (e) => {
+    e.preventDefault();
+    setTask([...task, input]);
+    setInput("");
+  };
+
+  const removeHandler = (index) => {
+    let storedTask = [...task];
+    storedTask.splice(index, 1);
+    setTask(storedTask);
+  };
+
+  // const archive = ( archive ) => {
+  //   let loggedTask = [ ...task ]
+  //   loggedTask.append(i)
+  // }
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      setTask([...task, input]);
+      setInput("");
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="Heading" >
+      <h1 className="Title">To-Do List</h1>
+      </div>
+      <div className="Bar">
+      <input
+        className="Tasks"
+        type="text"
+        value={input}
+        placeholder="Eg. Buy eggs"
+        onKeyDown={handleKeyDown}
+        onChange={(e) => setInput(e.target.value)}
+      ></input>
+      <button type="button" className="Add" onClick={submitHandler}>
+        Add
+      </button>
+      </div>
+      {task.map((task, index, archive) => (
+        <ToDo
+          key={ task }
+          index={ index }
+          task={ task }
+          removeHandler={ removeHandler } />
+      ) ) }
+          <div className="Subheader">
+            <h1 className="Title">Archived</h1>
+          </div>
+    </div>
+  );
+};
 
 export default App;
---
+++ src/components/TodoList.js
@@ -0,0 +1,38 @@
+const { useState } = require("react");
+
+const ToDo = ({ task, removeHandler, archive, index }) => {
+  const [complete, setComplete] = useState(false);
+
+  return (
+    <div className="Options">
+      {complete ? (
+        <h2 className="taskComplete">{task}</h2>
+      ) : (
+        <h2 className="New-Task">{task}</h2>
+      )}
+      <button
+        type="button"
+        className="Remove"
+        onClick={() => removeHandler(index)}
+      >
+        Remove Task
+      </button>
+      <button
+        type="button"
+        className="Complete"
+        onClick={() => setComplete(true)}
+      >
+        Completed
+      </button>
+      <button
+        type="button"
+        className="Archive"
+        onClick={() => archive(archive)}
+      >
+        Archive
+      </button>
+    </div>
+  );
+};
+
+export default ToDo;
